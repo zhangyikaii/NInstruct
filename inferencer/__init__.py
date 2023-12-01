f6c8311c84d392da78fa74521fdbbcf8ef8ef2f1
Yi-Kai Zhang <zykycy@gmail.com>
2023-12-02 07:54:16 +0800

    Review __init__.py

--- inferencer/__init__.py
+++ inferencer/__init__.py
@@ -1,6 +1,7 @@
 from inferencer.base import BaseInferencer
 from inferencer.meichichina import MeishiChinaInferencer
 from inferencer.daydaycook import DaydaycookInferencer
+from inferencer.douguo import DouGuoInferencer
 from inferencer.xiachufang import XiachufangInferencer
 from inferencer.xinshipu import XinshipuInferencer
 
@@ -8,6 +9,7 @@ __all__ = [
     'BaseInferencer',
     'MeishiChinaInferencer',
     'DaydaycookInferencer',
+    'DouGuoInferencer',
     'XiachufangInferencer',
     'XinshipuInferencer'
     ]
@@ -15,6 +17,7 @@ __all__ = [
 EXP_STR2CLASS_NAME = {
     'meishichina': 'MeishiChinaInferencer',
     'daydaycook': 'DaydaycookInferencer',
+    'douguo':'DouGuoInferencer',
     'xiachufang': 'XiachufangInferencer',
     'xinshipu': 'XinshipuInferencer'
 }
