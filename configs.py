a015631217d9c1fa8027ec1a154b1589db2892a2
zhongxvxiang <77152093+zhongxvxiang@users.noreply.github.com>
2023-12-02 02:05:10 +0800

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,9 +1,10 @@
 from pathlib import Path
 
 DATA_PATHS = {
-    'meishichina': f'data/meishichina',
-    'daydaycook' : f'data/daydaycook',
-    'douguo': f'data/douguo'
+    'meishichina': f'data',
+    'daydaycook': f'data/daydaycook',
+    'xiachufang': f'data/xiachufang',
+    'xinshipu': f'data/xinshipu'
 }
 
 IMG_SAVE_PATH = 'results/imgs'
