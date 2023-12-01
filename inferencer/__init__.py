5327d6fde59b9f5639c4ab6a1ddc59acdc5661ba
zhongxvxiang <77152093+zhongxvxiang@users.noreply.github.com>
2023-12-02 01:54:15 +0800

    Update __init__.py

--- inferencer/__init__.py
+++ inferencer/__init__.py
@@ -1,17 +1,20 @@
 from inferencer.base import BaseInferencer
 from inferencer.meichichina import MeishiChinaInferencer
 from inferencer.daydaycook import DaydaycookInferencer
-from inferencer.douguo import DouGuoInferencer
+from inferencer.xiachufang import XiachufangInferencer
+from inferencer.xinshipu import XinshipuInferencer
 
 __all__ = [
     'BaseInferencer',
     'MeishiChinaInferencer',
     'DaydaycookInferencer',
-    'DouGuoInferencer'
+    'XiachufangInferencer',
+    'XinshipuInferencer'
     ]
 
 EXP_STR2CLASS_NAME = {
     'meishichina': 'MeishiChinaInferencer',
     'daydaycook': 'DaydaycookInferencer',
-    'douguo':'DouGuoInferencer',
+    'xiachufang': 'XiachufangInferencer',
+    'xinshipu': 'XinshipuInferencer'
 }
